12f6a753de33b4b176269a049081ec93f2ec82fc
rarayukawa <r1u2u18@gmail.com>
2021-03-06 12:13:37 +0000

    [start]応用課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,27 @@
 class FavoritesController < ApplicationController
+before_action :authenticate_user!
+before_action :set_book
 
   def create
+    # Favorite.create(user_id: current_user.id, book_id: params[:id])
     @book = Book.find(params[:book_id])
     @favorites = @book.favorites.all
-    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite = current_user.favorites.build(book_id: params[:book_id])
     @favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    # Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
     @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite = Favorites.find_by(book_id: params[:book_id], user_id: current_user.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
+
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following_user, through: :follower, source: :followed #自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower #自分をフォローしている人
-  
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
@@ -21,14 +21,17 @@ class User < ApplicationRecord
  def follow(user_id)
    follower.create(followed_id: user_id)
  end
- 
+
  def unfollow(user_id)
    follower.find_by(followed_id: user_id).destroy
  end
- 
+
  def following?(user)
    following_user.include?(user)
  end
- 
- 
+
+
+ def favorited_by?(book_id)
+   favorites.where(book_id: book_id).exists?
+ end
 end
